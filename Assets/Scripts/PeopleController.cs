e7cc0a70aba92ecb2b604d6cb39f666eff68ea8a
DaSpikeyBoss <55066633+DaSpikeyBos@users.noreply.github.com>
2022-11-22 19:46:12 -0800

    Added movement towards a target

--- Assets/Scripts/PeopleController.cs
+++ Assets/Scripts/PeopleController.cs
@@ -4,15 +4,38 @@ using UnityEngine;
 
 public class PeopleController : MonoBehaviour
 {
+    public float personSpeed = 1;
+
+    public float viewportWidth = 5;
+    public float viewportHeight = 3;
+
+    Vector3 targetPosition;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        ChooseNewTarget();
+    }
+
+    void ChooseNewTarget() {
+        targetPosition = new Vector3(
+            Random.Range(-viewportWidth, viewportWidth),
+            Random.Range(-viewportHeight, viewportHeight)
+        );
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 toTarget = targetPosition - transform.position;
+        float sqrDistance = toTarget.sqrMagnitude;
+
+        if (sqrDistance < 0.1f) {
+            ChooseNewTarget();
+        }
+
+        // Move towards target
+        transform.position = Vector3.MoveTowards(
+            transform.position, targetPosition, personSpeed * Time.deltaTime);
     }
 }
