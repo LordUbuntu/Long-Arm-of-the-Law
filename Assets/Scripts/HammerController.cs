42036035e2a397a1eb8bac2387c14e2e0c3b05cd
Jacobus Burger <therealjacoburger@gmail.com>
2022-11-28 15:35:46 -0800

    added Hammer controller placeholder code

--- Assets/Scripts/HammerController.cs
+++ Assets/Scripts/HammerController.cs
@@ -6,4 +6,23 @@ public class HammerController : MonoBehaviour {
     // TODO
     // - have an arm connecting it to it's "body" (PlayerController)
     // - detect overlapping entities on lmb and score them
+
+    // placeholder
+    public int score = 0;
+
+
+    void Update() {
+        Collider[] overlap = Physics.OverlapSphere(transform.position, 0f);
+        
+        // for each object in collision area
+        foreach (Collider hit in overlap) {
+            // score hits
+            if (hit.gameObject.tag == "enemy") {
+                score = score - 1
+            } else
+            if (hit.gameObject.tag == "civilian") {
+                score = score + 1
+            }
+        }
+    }
 }
